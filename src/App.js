e52e12e00aea3f2f919cb2b50889737e954468f7
Sophiee43 <126475876+Sophiee43@users.noreply.github.com>
2023-05-31 02:17:12 +1300

    Updated my App.js with my github url

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Weather App
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sophie and is  
+      <a href="https://github.com/Sophiee43/project-react.git">Open-sourced on github </a>
+      </footer>
     </div>
   );
 }
