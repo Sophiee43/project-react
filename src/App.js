68d2a46965ccba99a93a80c16b6402ee7c6a25b7
Sophiee43 <126475876+Sophiee43@users.noreply.github.com>
2023-05-31 02:24:57 +1300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">      <h1>Weather App</h1>
       <footer>
         This project was coded by Sophie and is  
       <a href="https://github.com/Sophiee43/project-react.git">Open-sourced on github </a>
       </footer>
+      </div>
+
     </div>
   );
 }
