ecce36e0addf8e1d639d136e86a61a7d29e72a89
Sophiee43 <126475876+Sophiee43@users.noreply.github.com>
2023-06-13 10:03:37 +1300

    Added my Search Engine to the weather App

--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,12 @@
 import React,{ useState } from "react";
 import "./weather.css";
-import Date from "./formattedDate"
+
+
+import WeatherInfo from "./WeatherInfo"
 import axios from "axios";
 
 export default function Weather(props){
-
+const [city ,setCity]=useState(props.defaultCity)
 const[weatherData,setWeatherData]=useState({ready :false});
 function handleResponse(response) {
   console.log(response.data);
@@ -20,16 +22,31 @@ function handleResponse(response) {
   })
   ;
 }
+function Search (){
+
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+}
+function handleSubmit(event){
+event.preventDefault();
+Search();
+}
+function handleCityChange(event){
+setCity(event.target.value)
+}
 if(weatherData.ready){
 return( 
 <div className="weather">
-  <form>
+  <form onSubmit={handleSubmit}>
   <div className="row">
   <div className="col-9">
   
    <input type="search" placeholder="Enter a city.."
    className="form-control" 
-   autoFocus="on"/>
+   autoFocus="on"
+   onChange={handleCityChange}/>
    </div>
    <div className="col-3">
    <input type="submit" value="Search" className="btn btn-primary w-100"/>
@@ -37,40 +54,13 @@ return(
    </div>
    </div>
    </form>
-   <h1>{weatherData.city}</h1>
-   <ul>
-    <li><Date date={weatherData.date}/></li>
-    <li className="text-capitalize">{weatherData.description}</li>
-   </ul>
-  <div className="row mt-3">
-   <div className="col-6">
-    <div className="clearfix">
-    <img src={weatherData.iconUrl}alt={weatherData.description}className="float-left"
-    />
-    <div className="float-left">
-    <span className="temperature">{Math.round(weatherData.temperature)}</span>
-    <span className="unit">℃</span>
-    </div>
-    </div>
-   </div>
-   <div className="col-6">
-    <ul>
-      <li>Precipitation : 15%</li>
-      <li>Humidity:{weatherData.humidity}</li>
-      <li>Wind:{weatherData.wind}km/h</li>
-    </ul>
-   </div>
-  </div>
-</div>);
+   <WeatherInfo data={weatherData}/>
+   </div>);
 }
 else{
+Search();
+return "Loading"
 
-
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let city="lagos"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
- return "Loading...";
 }
 
 
